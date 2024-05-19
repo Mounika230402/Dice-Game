b37152592e8360ad9a0c30bd3c93f3b4b6686e61
Mounika Kankanampati <kankanampatimounika@gmail.com>
2024-05-19 17:50:14 +0530

    file

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var randomNumber1= Math.floor(Math.random()*6)+1;
+
+
+var getBtn=document.querySelector("#btn")
+
+getBtn.addEventListener("click",()=>{
+    var randomNumber1= Math.floor(Math.random()*6)+1;
 var randomDice1="dice"+randomNumber1+".png";
 var Source1="./images/"+randomDice1;
 var image1=document.querySelector(".img1");
@@ -9,7 +14,6 @@ var randomDice2="dice"+randomNumber2+".png";
 var Source2="./images/"+randomDice2;
 var image2=document.querySelector(".img2");
 image2.setAttribute("src",Source2);
-
 
 if (randomNumber1 > randomNumber2)
 {
@@ -22,4 +26,5 @@ else if(randomNumber1 < randomNumber2)
 else{
     document.querySelector("h1").innerHTML="Both are draw";
 }
+})
 //alert("Welcome to Dice game")
